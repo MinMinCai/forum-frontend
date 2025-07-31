baccde1af553c603ed09d553f56306690adecc06
MinMinCai <95090103+MinMinCai@users.noreply.github.com>
2025-07-31 10:14:54 +0800

    Add Vue file

---
+++ src/router/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue') // 動態載入(若使用者未進入signup，根本不用先載入signup頁面)
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/UsersTop.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound,
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
